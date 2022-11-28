6fbc6341a3eb02df4ec5b46c867ab5d7b731bcf6
clmb00 <paolo.colombo2000@gmail.com>
2022-11-28 10:34:45 +0100

    [add] article in blog ad component

---
+++ src/components/BlogArticle.vue
@@ -0,0 +1,91 @@
+<script>
+
+export default{
+  name: 'BlogArticle',
+  props: {
+    title: String,
+    author: String,
+    dateDay: Number,
+    dateMonth: String,
+    imgBack: String
+  }
+}
+</script>
+
+<template>
+
+  <article>
+    <img :src="'src/assets/img/' + imgBack" :alt="imgBack">
+    <h3>{{title}}</h3>
+    <p>{{author}}</p>
+    <div class="tag">
+      <p class="accent_text">{{dateDay}}</p>
+      <p>{{dateMonth}}</p>
+    </div>
+  </article>
+
+</template>
+
+<style lang="scss" scoped>
+
+@use '../styles/partials/variables' as *;
+
+article{
+  width: 32%;
+  position: relative;
+  cursor: pointer;
+  img{
+    max-height: 260px;
+  }
+  h3{
+    font-weight: 400;
+    font-size: 1.4rem;
+    margin-top: 24px;
+  }
+  &>p{
+    font-style: italic;
+    font-family: "Playfair Display", sans-serif;
+    font-size: .8rem;
+    margin-top: 15px;
+  }
+  .tag{
+    position: absolute;
+    top: -20px;
+    left: 20px;
+    text-align: center;
+    p:first-child{
+      font-size: 2.1rem;
+      background-color: $back-green-dark;
+      padding: 0px 12px 10px;
+      line-height: 2.2rem;
+    }
+    p:last-child{
+      text-transform: uppercase;
+      font-size: .7rem;
+      font-weight: 700;
+      background-color: $back-pink-normal;
+      position: relative;
+      padding-block: 5px;
+      &::before{
+        content: '';
+        position: absolute;
+        top: 100%;
+        left: 0;
+        width: 50%;
+        height: 15px;
+        background: linear-gradient(to top left, rgba(0,0,0,0) 0%, rgba(0,0,0,0)48%, $black-trasparent 48%, $back-pink-normal 52%, $back-pink-normal 100%);
+      }
+      &::after{
+        content: '';
+        position: absolute;
+        top: 100%;
+        right: 0;
+        width: 50%;
+        height: 15px;
+        background: linear-gradient(to top right, rgba(0,0,0,0) 0%, rgba(0,0,0,0)48%, $black-trasparent 48%, $back-pink-normal 52%, $back-pink-normal 100%);
+      }
+    }
+  }
+}
+
+</style>
--- src/components/MainBlog.vue
+++ src/components/MainBlog.vue
@@ -1,7 +1,12 @@
 <script>
 
+import BlogArticle from './BlogArticle.vue';
+
 export default{
-  name: 'MainBlog'
+  name: 'MainBlog',
+  components:{
+    BlogArticle
+  }
 }
 
 </script>
@@ -12,35 +17,29 @@ export default{
 
     <div class="container">
 
-      <article>
-        <img src="../assets/img/h-2-blog-img-1.jpg" alt="Banana">
-        <h3>This way, loves: a detailed guide through new design</h3>
-        <p>By Emily Fields</p>
-        <div class="tag">
-          <p class="accent_text">07</p>
-          <p>MAY '19</p>
-        </div>
-      </article>
-  
-      <article>
-        <img src="../assets/img/h-2-blog-img-2.jpg" alt="Town">
-        <h3>I try to give people a different way of looking art</h3>
-        <p>By Emily Fields</p>
-        <div class="tag">
-          <p class="accent_text">07</p>
-          <p>MAY '19</p>
-        </div>
-      </article>
-  
-      <article>
-        <img src="../assets/img/h-2-blog-img-3.jpg" alt="Girl">
-        <h3>Introduce Richard Laperrière of those amazing features</h3>
-        <p>By Emily Fields</p>
-        <div class="tag">
-          <p class="accent_text">07</p>
-          <p>MAY '19</p>
-        </div>
-      </article>
+      <BlogArticle
+        title="This way, loves: a detailed guide through new design" 
+        author="By Emily Fields" 
+        dateDay="12" 
+        dateMonth="AUG '21" 
+        imgBack="h-2-blog-img-1.jpg"
+      />
+
+      <BlogArticle
+        title="I try to give people a different way of looking art" 
+        author="By Emily Fields" 
+        dateDay="07" 
+        dateMonth="JAN '20" 
+        imgBack="h-2-blog-img-2.jpg"
+      />
+
+      <BlogArticle
+        title="Introduce Richard Laperrière of those amazing features" 
+        author="By Emily Fields" 
+        dateDay="22" 
+        dateMonth="MAY '19" 
+        imgBack="h-2-blog-img-3.jpg"
+      />
 
     </div>
 
@@ -51,8 +50,6 @@ export default{
 
 <style lang="scss" scoped>
 
-@use '../styles/partials/variables' as *;
-
 #blog{
   padding-block: 125px;
 }
@@ -60,64 +57,6 @@ export default{
 .container{
   display: flex;
   justify-content: space-between;
-}
-
-article{
-  width: 32%;
-  position: relative;
-  cursor: pointer;
-  img{
-    max-height: 260px;
-  }
-  h3{
-    font-weight: 400;
-    font-size: 1.4rem;
-    margin-top: 24px;
-  }
-  &>p{
-    font-style: italic;
-    font-family: "Playfair Display", sans-serif;
-    font-size: .8rem;
-    margin-top: 15px;
-  }
-  .tag{
-    position: absolute;
-    top: -20px;
-    left: 20px;
-    text-align: center;
-    p:first-child{
-      font-size: 2.1rem;
-      background-color: $back-green-dark;
-      padding: 0px 12px 10px;
-      line-height: 2.2rem;
-    }
-    p:last-child{
-      text-transform: uppercase;
-      font-size: .7rem;
-      font-weight: 700;
-      background-color: $back-pink-normal;
-      position: relative;
-      padding-block: 5px;
-      &::before{
-        content: '';
-        position: absolute;
-        top: 100%;
-        left: 0;
-        width: 50%;
-        height: 15px;
-        background: linear-gradient(to top left, rgba(0,0,0,0) 0%, rgba(0,0,0,0)48%, $black-trasparent 48%, $back-pink-normal 52%, $back-pink-normal 100%);
-      }
-      &::after{
-        content: '';
-        position: absolute;
-        top: 100%;
-        right: 0;
-        width: 50%;
-        height: 15px;
-        background: linear-gradient(to top right, rgba(0,0,0,0) 0%, rgba(0,0,0,0)48%, $black-trasparent 48%, $back-pink-normal 52%, $back-pink-normal 100%);
-      }
-    }
-  }
 }
 
 </style>
